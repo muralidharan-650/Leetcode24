03699e2cefa2f32265dae164cc9efac923278b55
muralidharan-650 <139471928+muralidharan-650@users.noreply.github.com>
2024-08-01 23:03:42 +0530

    Time: 3 ms (83.6%), Space: 58.6 MB (71.6%) - LeetHub

--- 2722-prime-in-diagonal/2722-prime-in-diagonal.java
+++ 2722-prime-in-diagonal/2722-prime-in-diagonal.java
@@ -23,7 +23,7 @@ class Solution {
             if(n==1)
                 return false;
             else{
-            for(int i=2;i<=n/2;i++)
+            for(int i=2;i<=Math.sqrt(n);i++)
             {
                 if(n%i==0)
                     return false;
